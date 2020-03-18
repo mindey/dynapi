1a75dc5be56c5de9c00a84d2a23a460496e182e3
Mindey <mindey@qq.com>
2020-03-18 08:52:08 +0000

    make caching optional

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='dynapi',
-    version='0.0.1',
+    version='0.0.2',
     description='Extras for dyanlist API wrapper.',
     url='https://gitlab.com/mindey/dynapi',
     author='Mindey',
